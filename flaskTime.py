c9c60f58b1e00438638329bcb48704c9d42c77e5
Tae-Ho-Yoo <98486145+Tae-Ho-Yoo@users.noreply.github.com>
2024-04-28 14:35:40 -0400

    flask test done

---
+++ flaskTime.py
@@ -0,0 +1,4 @@
+import time
+import pytest
+from flask import Flask
+from flask.testing import FlaskClient
---
+++ test_flask.py
@@ -0,0 +1,33 @@
+import time
+import pytest
+from flask import Flask
+from flask.testing import FlaskClient
+    
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+        
+    @app.route('/')
+    def index():
+        return 'TEST'
+        
+    yield app
+    
+@pytest.fixture
+def client(app):
+    return app.test_client()
+    
+def test_response_time(client: FlaskClient):
+    requests = 100
+    response_time = 0
+    for i in range(requests):
+        start = time.time()
+        response = client.get('/')
+        end = time.time()
+        response_time += (end - start)
+            
+    avg_time = response_time / requests
+    print("\nAverage response time: " + str(avg_time))
+    
+if __name__ == "__main__":
+    pytest.main(["-v"])
