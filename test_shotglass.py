fdd841b659e7a7369395578cc2ebb1553245ae67
Tae-Ho-Yoo <98486145+Tae-Ho-Yoo@users.noreply.github.com>
2024-04-28 23:45:41 -0400

    new shotglass test

--- test_shotglass.py
+++ test_shotglass.py
@@ -1,33 +1,52 @@
-import time as t
+import socket
+import threading
+import time
 import pytest
 from shotglass import Shotglass
 
-app = Shotglass()
+class ShotglassClient:
+    def __init__(self, host = "0.0.0.0", port=80):
+        self.host = host
+        self.port = port
+    
+    def send_request(self, request):
+        start = time.time()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+            client.connect((self.host, self.port))
+            client.sendall(request.encode())
+            response = client.recv(1024)
+        end = time.time()
+        response_time = end - start
+        return response, response_time
 
-class fakeSocket:
-    def send(self, data):
-        pass
+@pytest.fixture
+def server():
+    server = Shotglass()
+    thread = threading.Thread(target=server.run)
+    thread.start()
+    
+    yield server
+    
+    server.server_socket.close()
+    
+@pytest.fixture
+def client():
+    return ShotglassClient()
 
-@app.route("/", "GET")
-def test_response_time():
-    request = 100
+def test_response_time(server, client):
+    requests = 100
+    total_response_time = 0
     
-    def get_request(socket):
-        data = "TEXT"
-        response = app.create_response(200, "text/html", data)
-        Shotglass.send_response(app, socket, response)
+    for i in range(requests):
+        request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n"
+        response, response_time = client.send_request(request)
+        total_response_time += response_time
     
-    start = t.time()
-    
-    for i in range(request):
-        get_request(fakeSocket())
-        
-    end = t.time()
-    
-    time = (end - start) / request
-    num_get_request_per_sec = int(60 / time)
-    print("\nAverage response time for shotglass: " + str(time))
-    print("Number of GET request for shotglass per second: " + str(num_get_request_per_sec))
+    avg_response_time = total_response_time / requests
+    print("Average response time for shotglass: " + str(avg_response_time))
+    num_request_per_sec = int(60 / avg_response_time)
+    print("Number of GET request for shotglass per second: " + str(num_request_per_sec))
 
 if __name__ == "__main__":
     pytest.main(["-v"])
+        
